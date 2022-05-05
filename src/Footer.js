b6d27af1a27ff01f8b0e972795c43ee75fc93eaa
laurapurser <90283298+laurapurser@users.noreply.github.com>
2022-05-05 20:15:12 +0100

    Created Footer

---
+++ src/Footer.js
@@ -0,0 +1,18 @@
+import React from "react";
+import "./Footer.css";
+
+export default function Footer() {
+  return (
+    <footer>
+      <a
+        href="https://github.com/laurapurser/Weather-Project"
+        target="_blank"
+        rel="noreferrer"
+      >
+        {" "}
+        Open Source Code{" "}
+      </a>{" "}
+      by Laura Purser
+    </footer>
+  );
+}
