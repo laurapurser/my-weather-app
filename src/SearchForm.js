00924c1a98305e5b594a91af1603aa6cd1b613cd
laurapurser <90283298+laurapurser@users.noreply.github.com>
2022-05-09 11:46:51 +0100

    Combined to make search engine

--- src/SearchForm.js
+++ src/SearchForm.js
@@ -1,31 +1,70 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
 import SearchResults from "./SearchResults";
 import "./SearchResults.css";
-
 import "./SearchForm.css";
 
-export default function SearchForm() {
-  return (
-    <div>
-      <h2 className="SearchForm">
-        <form id="search-form">
-          <input
-            type="text"
-            id="search-text-input"
-            placeholder="Enter a city"
-            className="search-field"
-          />{" "}
-          <input type="submit" value="Search" className="submit-button" />
-          <button
-            type="button"
-            id="current-weather"
-            className="current-weather"
-          >
-            Current
-          </button>{" "}
-        </form>
-      </h2>
-      <SearchResults />
-    </div>
-  );
+export default function SearchForm(props) {
+  const [city, setCity] = useState(props.defaultCity);
+
+  const [weatherData, setWeatherData] = useState({ ready: false });
+
+  function handleResponse(response) {
+    setWeatherData({
+      ready: true,
+      city: response.data.name,
+      date: new Date(response.data.dt * 1000),
+      feels: response.data.main.feels_like,
+      temp: response.data.main.temp,
+      current: response.data.weather[0].description,
+      humidity: response.data.main.humidity,
+      min: response.data.main.temp_min,
+      max: response.data.main.temp_max,
+      wind: response.data.wind.speed,
+    });
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    searchCity();
+  }
+
+  function handleCityChange(event) {
+    setCity(event.target.value);
+  }
+
+  function searchCity() {
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=082d3d02ffdb12f2fd9b259e2ced1d0d&units=metric`;
+    axios.get(apiUrl).then(handleResponse);
+  }
+
+  if (weatherData.ready) {
+    return (
+      <div>
+        <h2 className="SearchForm">
+          <form id="search-form" onSubmit={handleSubmit}>
+            <input
+              type="text"
+              id="search-text-input"
+              placeholder="Enter a city"
+              className="search-field"
+              onChange={handleCityChange}
+            />{" "}
+            <input type="submit" value="Search" className="submit-button" />
+            <button
+              type="button"
+              id="current-weather"
+              className="current-weather"
+            >
+              Current
+            </button>{" "}
+          </form>
+        </h2>
+        <SearchResults data={weatherData} />
+      </div>
+    );
+  } else {
+    searchCity();
+    return "Loading...";
+  }
 }
--- src/SearchResults.js
+++ src/SearchResults.js
@@ -1,85 +1,59 @@
-import React, { useState } from "react";
+import React from "react";
 import "./SearchResults.css";
-import axios from "axios";
 import FormattedDate from "./FormattedDate.js";
 
-export default function SearchResults() {
-  const [ready, setReady] = useState(false);
-  const [weatherData, setWeatherData] = useState("");
+export default function SearchResults(props) {
+  return (
+    <div className="row results">
+      <div className="col-6">
+        <div className="card">
+          <div className="card-body">
+            <ul>
+              <li className="card-title" id="cardtitle">
+                {props.data.city}
+              </li>
 
-  function handleResponse(response) {
-    setReady(true);
-    setWeatherData({
-      city: "New York",
-      date: new Date(response.data.dt * 1000),
-      feels: response.data.main.feels_like,
-      temp: response.data.main.temp,
-      current: response.data.weather[0].description,
-      humidity: response.data.main.humidity,
-      min: response.data.main.temp_min,
-      max: response.data.main.temp_max,
-      wind: response.data.wind.speed,
-    });
-  }
-  if (ready) {
-    return (
-      <div className="row results">
-        <div className="col-6">
-          <div className="card">
-            <div className="card-body">
-              <ul>
-                <li className="card-title" id="cardtitle">
-                  {weatherData.city}
-                </li>
+              <li className="cardtexttwo" id="date-line">
+                <FormattedDate date={props.data.date} />
+              </li>
 
-                <li className="cardtexttwo" id="date-line">
-                  <FormattedDate date={weatherData.date} />
-                </li>
+              <li className="cardtextthree" id="temp-number">
+                {Math.round(props.data.temp)}°C
+              </li>
 
-                <li className="cardtextthree" id="temp-number">
-                  {Math.round(weatherData.temp)}°C
-                </li>
-
-                <li className="cardtext" id="feelslike">
-                  Feels like: {Math.round(weatherData.feels)}°C
-                </li>
-              </ul>
-            </div>
-          </div>
-        </div>
-
-        <div className="col-6">
-          <div className="card card-one">
-            <div className="card-body">
-              <ul>
-                <li className="cardtext text-capitalize">
-                  Current view: {weatherData.current}{" "}
-                  <span id="description" class="describe"></span>
-                </li>
-                <li className="cardtext" id="actual-humidity">
-                  Humidity: {weatherData.humidity}%
-                </li>
-                <li className="cardtext" id="high">
-                  High: {Math.round(weatherData.max)}°C
-                </li>
-                <li className="cardtext" id="low">
-                  Low: {Math.round(weatherData.min)}°C
-                </li>
-
-                <li className="cardtext" id="windspeed">
-                  Windspeed: {Math.round(weatherData.wind)}mph
-                </li>
-              </ul>
-            </div>
+              <li className="cardtext" id="feelslike">
+                Feels like: {Math.round(props.data.feels)}°C
+              </li>
+            </ul>
           </div>
         </div>
       </div>
-    );
-  } else {
-    let city = "New York";
-    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=082d3d02ffdb12f2fd9b259e2ced1d0d&units=metric`;
 
-    axios.get(apiUrl).then(handleResponse);
-    return "Loading....";
-  }
+      <div className="col-6">
+        <div className="card card-one">
+          <div className="card-body">
+            <ul>
+              <li className="cardtext text-capitalize">
+                Current view: {props.data.current}{" "}
+                <span id="description" class="describe"></span>
+              </li>
+              <li className="cardtext" id="actual-humidity">
+                Humidity: {props.data.humidity}%
+              </li>
+              <li className="cardtext" id="high">
+                High: {Math.round(props.data.max)}°C
+              </li>
+              <li className="cardtext" id="low">
+                Low: {Math.round(props.data.min)}°C
+              </li>
+
+              <li className="cardtext" id="windspeed">
+                Windspeed: {Math.round(props.data.wind)}mph
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
